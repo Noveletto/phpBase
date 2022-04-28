ac32430c19b8802b7390f47a8d21d0f7895abd8f
Noveletto <guh.noveletto@outlook.com>
2022-04-28 11:18:30 -0300

    corrigindo sql city

--- sqlCodigos/city.sql
+++ sqlCodigos/city.sql
@@ -1,5 +1,5 @@
 create table city(
-    ID_city primary key not null,
+    ID_city int primary key AUTO_INCREMENT not null,
     city varchar(40)
-)
+);
 insert into city (city) values ("Hortolandia"), ("Sumare"), ("Campinas");
