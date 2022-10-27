e930eaa9e73e2dbd79928280afce085c502e21bb
Noveletto <guh.noveletto@outlook.com>
2022-10-27 12:29:58 -0300

    gerar pdf

---
+++ relatorioClientes/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>DiamondTech Relatório Produtos</title>
+</head>
+<body>
+
+    <h1>Relatório Produtos - DiamondTech</h1>
+
+    <a href="gerar_pdf.php">Gerar PDF</a>
+    
+</body>
+</html>
